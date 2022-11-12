a12cc496942df0e44aea3111c920dea6737998a1
Ste1nbach <dansteinbach1@gmail.com>
2022-11-12 13:01:42 +0100

    class chicken

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.querySelector("canvas");
 const wrap = document.getElementById("wrap");
 const wrapper = document.getElementById("wrapper");
 const btn = document.getElementById("btn");
@@ -11,3 +11,41 @@ btn.onclick = () => {
     wrap.style.display = "none";
     canvas.style.display = "flex";
 }
+
+class chicken {
+    constructor() {
+        this.width = 50;
+        this.height = 50;
+        // this.img = new Image();
+        // this.path = "./res/img/chicken.jpg";
+        // this.img.src = this.path;
+        this.position = {
+            x: 200,
+            y: 200,
+        };
+    }
+
+    draw() {
+        ctx.fillStyle = "red";
+        ctx.fillRect( this.width, this.height, this.position.x, this.position.y);
+    }
+    update() {
+        this.draw();
+    }
+}
+
+const bot = new chicken();
+
+function botMovement() {
+    requestAnimationFrame(botMovement);
+    bot.update();
+
+    for(;bot.position.x < canvas.width;) {
+        bot.position.x = bot.position.x + 1;
+        console.log(bot.position.x);
+
+    }
+
+}
+
+botMovement();
