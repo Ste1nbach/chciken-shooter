764631a755b03f03279062bfbdf7adb2dd715678
Ste1nbach <dansteinbach1@gmail.com>
2023-03-11 19:21:06 +0100

    chicken click

--- res/js/chicken.js
+++ res/js/chicken.js
@@ -1,19 +1,22 @@
 import {canvas, wrap, btn, ctx, array} from "./canvas.js";
 
+let chickenRemove = new Image;
+
+chickenRemove.src = "./res/img/chicken-remove.png";
+
 export class Chicken {
-    constructor() {
+    constructor(xpos) {
         this.position = {
-            x: 50,
-            y: Math.random() * canvas.height,
+            x: xpos,
+            y: Math.floor(Math.random() * (500 - 0) + 0)
         };
-        this.width = 50;
-        this.height = 50;
-        this.count = []
+        this.width = 150;
+        this.height = 150;
     }
 
+
     draw() {
-        ctx.fillRect(this.position.x, this. position.y, this.width, this.height);
-        this.count.forEach(element => {element.update()});
+        ctx.drawImage(chickenRemove, this.position.x, this.position.y, this.width, this.height);
     }
     update() {
         this.draw();
--- res/js/main.js
+++ res/js/main.js
@@ -4,6 +4,7 @@ import { Chicken } from "./chicken.js";
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+
 let posY = Math.floor (Math.random() * canvas.height);
 let i = 0;
 
@@ -13,47 +14,37 @@ btn.onclick = () => {
     bot.position.x = 0;
 }
 
-
-
-const bot = new Chicken();
-
+const bot = new Chicken(50);
 
 const generate = () => {
-           i++;
-           
-        if(i % 430 == 0) {
-            do{
-                posY = Math.floor (Math.random() * canvas.height);
-                bot.position.y = posY;
-            }while(posY < 50 || posY > 300);
+
+        if(bot.position.x > canvas.width) {
+            posY = Math.floor(Math.random() * (500 - 0) + 0);
+            bot.position.y = posY;
             console.log("de");
             
-            bot.position.x = 50;
-            
-            
+            bot.position.x = 0;   
+            console.log("bot y " + posY);
+        console.log("bot x " + bot.position.x) 
     }
     bot.update();
-       
-        console.log(posY);
-    
-
 }
 
 
-const draw = () => {
-    for(let i = 0; i < 10; i++) {
-        ctx.fillRect(bot.position.x, bot.position.y, bot.width, bot.height);
+canvas.addEventListener('click', function(event) {
+    let clickX = event.clientX;
+    let clickY = event.clientY;
+
+    if((bot.position.x <= clickX && bot.position.x + bot.width >= clickX) && (bot.position.y <= clickY && bot.position.y + bot.height >= clickY)){
+        console.log("trefil jsem slepici");
     }
-}
-
-
-
+});
 
 
 function botMovement() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    bot.position.x += 5;
+    bot.position.x += 3;
     generate();
     requestAnimationFrame(botMovement);
 }
