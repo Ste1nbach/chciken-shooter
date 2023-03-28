aadedea41e318e42baf5714b4addd7d63d4d964e
Ste1nbach <dansteinbach1@gmail.com>
2023-03-28 19:08:29 +0200

    chicken

--- res/js/chicken.js
+++ res/js/chicken.js
@@ -1,22 +1,20 @@
-import {canvas, wrap, btn, ctx, array} from "./canvas.js";
+import {canvas, wrap, btn, ctx} from "./canvas.js";
 
-let chickenRemove = new Image;
-
-chickenRemove.src = "./res/img/chicken-remove.png";
 
 export class Chicken {
-    constructor(xpos) {
+    constructor(xpos, chicImg) {
         this.position = {
             x: xpos,
             y: Math.floor(Math.random() * (500 - 0) + 0)
         };
         this.width = 150;
         this.height = 150;
+        this.chicImg = chicImg;
     }
 
 
     draw() {
-        ctx.drawImage(chickenRemove, this.position.x, this.position.y, this.width, this.height);
+        ctx.drawImage(this.chicImg, this.position.x, this.position.y, this.width, this.height);
     }
     update() {
         this.draw();
--- res/js/main.js
+++ res/js/main.js
@@ -1,34 +1,59 @@
-import { canvas, wrap, btn, ctx, array} from "./canvas.js";
+import { canvas, wrap, btn, ctx} from "./canvas.js";
 import { Chicken } from "./chicken.js";
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let chickenRemove = new Image;
+let chickenRot = new Image;
+let start = false;
+
+chickenRemove.src = "./res/img/ptak.png";
+chickenRot.src = "./res/img/ptak2.png";
+
 
 let posY = Math.floor (Math.random() * canvas.height);
-let i = 0;
+
 
 btn.onclick = () => {
     wrap.style.display = "none";
     canvas.style.display = "flex";
-    bot.position.x = 0;
+    start = true;
 }
 
-const bot = new Chicken(50);
+const chicken =  [];
+const bot = new Chicken(0, chickenRemove);
+const bot2 = new Chicken(canvas.width, chickenRot);
+
+chicken.push(bot);
+chicken.push(bot2);
+
 
 const generate = () => {
 
-        if(bot.position.x > canvas.width) {
-            posY = Math.floor(Math.random() * (500 - 0) + 0);
-            bot.position.y = posY;
-            console.log("de");
-            
-            bot.position.x = 0;   
-            console.log("bot y " + posY);
-        console.log("bot x " + bot.position.x) 
-    }
-    bot.update();
+        for(let i = 0; i < chicken.length; i++){
+
+            if(i%2 == 0) {
+                if(chicken[i].position.x > canvas.width) {
+                    posY = Math.floor(Math.random() * (500 - 0) + 0);
+                    chicken[i].position.y = posY; 
+                    chicken[i].position.x = 0; 
+            }
+
+            } else {
+                if(chicken[i].position.x < 0) {
+                    posY = Math.floor(Math.random() * (500 - 0) + 0);
+                    chicken[i].position.y = posY; 
+                    chicken[i].position.x = canvas.width; 
+            }
+            }
+
+           
+        chicken[i].update();
+        }
+
 }
+
 
 
 canvas.addEventListener('click', function(event) {
@@ -42,11 +67,26 @@ canvas.addEventListener('click', function(event) {
 
 
 function botMovement() {
+    
+    if(start == true) {
 
+   
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    bot.position.x += 3;
+
+    for(let i = 0; i < chicken.length; i++) {
+        if(i%2 == 0) {
+            chicken[i].position.x += 3;
+        } else {
+            chicken[i].position.x -= 3;
+        }
+    }
+
     generate();
-    requestAnimationFrame(botMovement);
+   
+ }
+ requestAnimationFrame(botMovement);
+ console.log(start);
+    
 }
 
 botMovement();
