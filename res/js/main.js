79815d61c182b5d554d86cecb5ffdd2a1d3d887e
Ste1nbach <dansteinbach1@gmail.com>
2023-04-03 20:30:53 +0200

    generate chickens

--- res/js/main.js
+++ res/js/main.js
@@ -1,23 +1,34 @@
 import { canvas, wrap, btn, ctx } from "./canvas.js";
 import { Chicken } from "./chicken.js";
+import { Airplane } from "./plane.js";
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let chickenRemove = new Image;
 let chickenRot = new Image;
-const chicken = [];
-let start = false;
-let pts = 0;
-let HP = 1;
-let death = false; 
+let chickenGolden = new Image;
+let planeImg = new Image;
 
 chickenRemove.src = "./res/img/ptak-remove.png";
 chickenRot.src = "./res/img/ptak2-remove.png";
+chickenGolden.src = "./res/img/golden2.png";
+planeImg.src = "./res/img/letadlo2.png";
 
-
+let chicken = [];
+let arrayLength = 10;
+let start = false;
+let pts = 0;
+let HP = 10;
+let death = false;
+let goldenSpawn = false;
+let planeSpawn = false;
+let chance = Math.floor(Math.random() * 5000);
+let chance2 = Math.floor(Math.random() * 2000);
 let posY = Math.floor(Math.random() * canvas.height);
 
+//const golden = new GoldenChicken(0, chickenGolden, "leftToRight");
+const plane2 = new Airplane(0, planeImg, "rightToLeft");
 
 btn.onclick = () => {
     wrap.style.display = "none";
@@ -25,59 +36,35 @@ btn.onclick = () => {
     start = true;
 }
 
-
-/*
-const bot = new Chicken(0, chickenRemove);
-const bot2 = new Chicken(canvas.width, chickenRot);
-
-chicken.push(bot);
-chicken.push(bot2);
-*/
-
-// nekonecny slepice dokud HP se nebudou rovnat nule
-
 const generate = () => {
 
-    if (HP == 0) {
-        start = false;
-        death = true;
-       for(let i = 0; i < chicken.length; i++) {
-        chicken.splice(i, 0);
-        chicken.splice(0, i);
-       }
-        
-        if(death = true) {
-            ctx.fillStyle = "white";
-            ctx.font = "bold 30px sans-serif";
-            ctx.fillText("GAME OVER PRESS SPACE TO RESTART", canvas.width / 2 - 250, canvas.height / 2);
-        }
-        
-    }
-
-    
-    if(start == false) {
-        window.addEventListener("keydown", (e) => {
-            switch (e.key) {
-              case " ":
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                start = true;
-                death = false;
-                console.log(e.key);
-                console.log(death);
-                console.log(start);
-                break;
-            }
-          });
-    }
-    
-
     for (let i = 0; i < chicken.length; i++) {
+
+        chance = Math.floor(Math.random() * 5000);
+        chance2 = Math.floor(Math.random() * 2000);
+
+        if (chance == 5 && goldenSpawn == false) {
+            posY = Math.floor(Math.random() * (500 - 0) + 0);
+            goldenSpawn = true;
+            chicken.push(new Chicken(-150 * i, chickenGolden, "leftToRight", "golden"));
+        }
+
+        if (chance2 == 5 && planeSpawn == false) {
+            posY = Math.floor(Math.random() * (500 - 0) + 0);
+            plane2.position.y = posY;
+            plane2.position.x = canvas.width;
+            planeSpawn = true;
+        }
+
+        if (planeSpawn == true) {
+            plane2.update();
+        }
 
         if (chicken[i].direction == "leftToRight") {
             if (chicken[i].position.x > canvas.width) {
                 posY = Math.floor(Math.random() * (500 - 0) + 0);
                 chicken[i].position.y = posY;
-                chicken[i].position.x = 0;
+                chicken[i].position.x = canvas.width;
                 HP--;
             }
 
@@ -89,8 +76,24 @@ const generate = () => {
                 HP--;
             }
         }
-
         chicken[i].update();
+    }
+
+}
+
+const end = () => {
+
+    if (HP == 0) {
+        start = false;
+        death = true;
+
+        if (death == true) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = "white";
+            ctx.font = "bold 30px sans-serif";
+            ctx.fillText("GAME OVER PRESS SPACE TO RESTART", canvas.width / 2 - 250, canvas.height / 2);
+        }
+
     }
 
 }
@@ -117,22 +120,58 @@ canvas.addEventListener('click', function (event) {
 
     for (let i = 0; i < chicken.length; i++) {
         if ((chicken[i].position.x <= clickX && chicken[i].position.x + chicken[i].width >= clickX) && (chicken[i].position.y <= clickY && chicken[i].position.y + chicken[i].height >= clickY)) {
-            console.log("trefil jsem slepici");
-            chicken.splice(i, 1);
-            pts++;
+            if (chicken[i].rarity == "golden") {
+                pts = pts + 5;
+                if (chicken[i].direction == "leftToRight") {
+                    goldenSpawn = false;
+                    generate();
+                }
+                chicken.splice(i, 1);
+            } else {
+                pts++;
+                if (chicken[i].direction == "leftToRight") {
+                    generateLeftToRight(i + 3);
+                    console.log(chicken.length);
+                } else {
+                    generateRightToLeft(i + 3);
+                    console.log(chicken.length);
+                }
+                chicken.splice(i, 1);
+            }
         }
     }
+
 });
 
+const generateLeftToRight = (i) => {
+    chicken.push(new Chicken(-150 * i, chickenRemove, "leftToRight", "basic"));
+}
+
+const generateRightToLeft = (i) => {
+    chicken.push(new Chicken(canvas.width + i * 150, chickenRot, "rightToLeft", "basic"));
+}
+
 const generateChickens = () => {
-    for (let i = 1; i <= 20 + pts; i++) {
+    for (let i = 0; i < arrayLength; i++) {
         if (i % 2 == 0) {
-            chicken.push(new Chicken(canvas.width + i * 150, chickenRot, "rightToLeft"));
-        } else {
-            chicken.push(new Chicken(-150 * i, chickenRemove, "leftToRight"));
+            generateRightToLeft(i);
+        }
+        else {
+            generateLeftToRight(i);
         }
     }
 }
+
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case " ":
+            start = true;
+            death = false;
+            location.reload();
+            break;
+    }
+});
+
 
 function gameLoop() {
 
@@ -149,17 +188,20 @@ function gameLoop() {
             } else {
                 chicken[i].position.x -= 3;
             }
+
+
+
         }
+        // golden.position.x += 3;
+        plane2.position.x -= 3;
 
         generate();
     }
     requestAnimationFrame(gameLoop);
     score();
     hp();
+    end();
 }
 
-if(start = true) {
-    generateChickens();
-}
-
+generateChickens();
 gameLoop();
--- res/js/plane.js
+++ res/js/plane.js
@@ -1,22 +1,20 @@
 import { ctx } from "./canvas.js";
 
-
-export class Chicken {
-    constructor(xpos, chicImg, direction, rarity) {
+export class Airplane {
+    constructor(xpos, planeImg, direction) {
         this.position = {
             x: xpos,
             y: Math.floor(Math.random() * (500 - 0) + 0)
         };
         this.width = 150;
         this.height = 150;
-        this.chicImg = chicImg;
+        this.planeImg = planeImg;
         this.direction = direction;
-        this.rarity = rarity;
     }
 
 
     draw() {
-        ctx.drawImage(this.chicImg, this.position.x, this.position.y, this.width, this.height);
+        ctx.drawImage(this.planeImg, this.position.x, this.position.y, this.width, this.height);
     }
     update() {
         this.draw();
